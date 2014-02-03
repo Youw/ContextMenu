680106e7933b91a8acd3539545ea6a543a7f95fd
Youw <ihor.youw@gmail.com>
2014-02-03 02:58:38 +0200

    now commit...

--- FilesInfoAndChecksum/JobsExecuting.h
+++ FilesInfoAndChecksum/JobsExecuting.h
@@ -36,15 +36,49 @@ namespace jobs{
 	};
 
 	template<typename RetType>
+	//performing single gob execution in paralel thread
+	//
+	class JobExecutor {
+		//just creating working thread
+		JobExecutor();
+
+		//creating working thread and start executing newJob
+		JobExecutor(const Job<RetType>& newJob);
+
+		//waiting for job to be done
+		//snd closing working thread
+		~JobExecutor();
+
+		//method is waiting until previous job will be done
+		//and than start execute newJob (not waiting for in to be done)
+		void AddJob(const Jobs<RetType>& newJobs);
+
+		//true if the job not executed yet
+		bool JobIsExecuting() const;
+
+		//waiting until job will be done
+		void WaitForJobDone() const;
+
+		//true it job is done
+		bool JobIsDone() const;
+
+		//if job is not done jet, waiting until it be done 
+		//than return result of the job 
+		RetType JobResult() const;
+	private:
+
+	};
+
+	template<typename RetType>
 	//Performing executing of jobs
-	//Using pool thread to execute jobs concurrency
+	//Using pool thread model to execute jobs concurrency
 	class JobsExecuter
 	{
 	public:
-		//just creating working threads, suspend them and wait for jobs
+		//just creating JobExecutors and wait for jobs
 		JobsExecuter();
 		
-		//creating working  threads and start execute new jobs
+		//creating JobExecutors and allocates Jobs among them
 		JobsExecuter(const Jobs<RetType>& newJobs);
 		
 		//waiting for all jobs to be done
@@ -52,7 +86,7 @@ namespace jobs{
 		~JobsExecuter();
 
 		//method is waiting until previous jobs will be done
-		//and than start execute new jobs and return
+		//and than start execute newJobs (not waiting for them done)
 		void AddJobs(const Jobs<RetType>& newJobs);
 
 		//true if there is any job not executed yet
@@ -61,13 +95,13 @@ namespace jobs{
 		//waiting until all jobs will be done
 		void WaitForJobsDone() const;
 
-		//true it job ¹ JobNum is done
+		//true it job # JobNum is done
 		bool JobIsDone(const size_t& jobNum) const;
 
 		//return current jobs count
 		size_t JobsCount() const;
 
-		//if job ¹ jobNum is not done jet, waiting until it be done 
+		//if job # jobNum is not done jet, waiting until it be done 
 		//than return result of the job 
 		RetType JobResult(const size_t& jobNum) const;
 
@@ -75,11 +109,12 @@ namespace jobs{
 		RetType operator[] (const size_t& jobNum) const;
 
 	private:
-		Jobs CurrentJobs;
-		std::vector<std::thread> WorkingThreads;
+		Jobs _CurrentJobs;
+		std::vector<JobExecutor> _WorkingThreads;
 	};//class JobsExecuter
 
 };//namespace jobs
 
 jobs::JobsExecuter<int> a;
+
 #endif //JOBEXECUTER_H
